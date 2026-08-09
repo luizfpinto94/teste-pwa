084ee26502395d1994ebbe168ca71c6751ab41c3
Luiz Felipe <luizfpinto94@gmail.com>
2026-08-09 15:42:32 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "meu-app-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES))
+    );
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(cachedResponse => {
+
+                return cachedResponse ||
+                    fetch(event.request);
+
+            })
+
+    );
+
+});
